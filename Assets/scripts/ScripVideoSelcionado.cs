a195d1c7ba0016a3785e365d2b9c319beb91f0db
LeoPereiravra <leopereiravra@gmail.com>
2019-11-20 21:04:31 -0300

    reversão para versão ok

---
+++ Assets/scripts/ScripVideoSelcionado.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+public class ScripVideoSelcionado : MonoBehaviour
+{
+    public RawImage imagem;
+    public List<VideoPlayer> Videos = new List<VideoPlayer>();
+    public static VideoPlayer vide;
+    
+    private VideoPlayer videoSelecionado;
+    public configHitoria historia;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+    }
+
+    void Update()
+    {        
+
+    }
+
+}
---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public bool shouldMove = false;
+    public configJogo config;
+    public MeshRenderer mesh;
+    private float timer;
+    private bool alterouVel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        alterouVel = false;
+        timer = config.timerLetras;
+        if (mesh != null)
+        {
+            float r = Random.Range(0.0f, 1f);
+            float g = Random.Range(0.0f, 1f);
+            float b = Random.Range(0.0f, 1f);
+            mesh.materials[0].color = new Color(r, g, b);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (config.trocaImg)
+        {
+            Destroy(this.GetComponent<GameObject>(),0.1f);
+        }
+        if (config.pause)
+        {
+            return;
+        }
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            if (!this.shouldMove)
+            {
+                transform.localPosition += Vector3.back * 1 / 24;
+                alterouVel = false;
+            }
+            else
+            {
+                transform.localPosition += Vector3.forward * 1 / 16;                
+                alterouVel = false;
+            }
+
+            timer = config.timerLetras;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !alterouVel)
+        {            
+            config.timerLetras = config.timerLetras - 0.01f;
+            if (config.timerLetras < 0.00f)
+            {
+                config.timerLetras = 0.00f;
+            }
+
+            alterouVel = true;
+        }
+        else
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !alterouVel)
+        {
+            config.timerLetras = config.timerLetras + 0.01f;
+            if (config.timerLetras > 0.05f)
+            {
+                config.timerLetras = 0.05f;
+            }
+            alterouVel = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+       
+        this.shouldMove = true;
+        Destroy(this);
+
+    }
+
+
+}
